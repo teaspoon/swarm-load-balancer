23bcea92ae1b125968b64be8bd51695bb412ef4d
teaspoon <kwaad.lepel@gmail.com>
2016-07-28 10:35:00 +0200

    make our app blue green

--- files/start.sh
+++ files/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-service nginx start
+nginx -g 'daemon off;' &
+echo -n $LIVE > /var/live
 consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/conf.d/default.conf:service nginx reload"
