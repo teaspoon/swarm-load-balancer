3850afa54602c20921e44f3f0a368562ef212c40
teaspoon <kwaad.lepel@gmail.com>
2016-08-28 21:16:41 +0200

    fix the start commannd to write over nginx.conf

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nginx -g 'daemon off;' &
 echo -n $LIVE > /var/live
-consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/conf.d/default.conf:service nginx reload"
+consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:service nginx reload"
